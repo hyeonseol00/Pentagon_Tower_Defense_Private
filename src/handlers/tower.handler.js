a6a6b99fd46a7027821cf1e752d7f6c5f2d63a26
hyeonseol00 <hyeonseol22@naver.com>
2024-06-18 20:46:15 +0900

    🎉 add : 타워 핸들러 추가

---
+++ src/handlers/tower.handler.js
@@ -0,0 +1,31 @@
+import { getGameAssets } from '../init/assets.js';
+import { getUserData, updateUserData } from '../models/user-data.model.js';
+
+export const placeInitialTowerHandler = (userId, payload) => {
+  const userData = getUserData(userId);
+  userData.tower_coordinates.push(payload.newTowerCoordinates);
+  updateUserData(userData);
+
+  return {
+    status: 'success',
+    message: '기본 타워 하나가 성공적으로 배치되었습니다.',
+  };
+};
+
+export const placeTowerHandler = (userId, payload) => {
+  const userData = getUserData(userId);
+  const { commonData } = getGameAssets();
+
+  if (userData.gold < commonData.tower_cost)
+    return { status: 'fail', message: '돈이 부족해 구매에 실패했습니다!' };
+
+  userData.gold -= 1000;
+  userData.tower_coordinates.push(payload.newTowerCoordinates);
+  updateUserData(userData);
+
+  return {
+    status: 'success',
+    message: '구매한 타워가 성공적으로 배치되었습니다.',
+    data: userData,
+  };
+};
