a2187ec58f320fac956932f5a455bcccaa32b4b6
hyeonseol00 <hyeonseol22@naver.com>
2024-06-18 20:47:55 +0900

    ✨ update : 핸들러 매핑

--- src/handlers/handlerMapping.js
+++ src/handlers/handlerMapping.js
@@ -1,7 +1,16 @@
-import { gameStart } from './game.handler.js';
+import { gameEnd, gameStart } from './game.handler.js';
+import { killMonsterHandler } from './monster.handler.js';
+import {
+  placeInitialTowerHandler,
+  placeTowerHandler,
+} from './tower.handler.js';
 
 const handlerMappings = {
   2: gameStart,
+  3: gameEnd,
+  21: placeInitialTowerHandler,
+  22: placeTowerHandler,
+  23: killMonsterHandler,
 };
 
 export default handlerMappings;
