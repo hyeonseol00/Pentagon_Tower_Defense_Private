98dc3135a0412f73ef0e73caf26bbc980dd89210
hyeonseol00 <hyeonseol22@naver.com>
2024-06-18 20:47:48 +0900

    ✨ update : 게임 종료 핸들러 함수 추가

--- src/handlers/game.handler.js
+++ src/handlers/game.handler.js
@@ -1,7 +1,13 @@
 import { getGameAssets } from '../init/assets.js';
 
-export const gameStart = async (uuid, payload) => {
-  const { stages } = getGameAssets();
+export const gameStart = (userId, payload) => {
+  const { templates } = getGameAssets();
 
   return { status: 'success', message: '게임이 정상적으로 실행되었습니다.' };
+};
+
+export const gameEnd = (userId, payload) => {
+  const { templates } = getGameAssets();
+
+  return { status: 'success', message: '게임 종료!' };
 };
