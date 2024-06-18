c05f73fa988e00e314017dcde4c3b916f784ba7c
hyeonseol00 <hyeonseol22@naver.com>
2024-06-18 15:53:22 +0900

    ✨ update : 게임 시작 이벤트 추가

--- src/handlers/handlerMapping.js
+++ src/handlers/handlerMapping.js
@@ -1,7 +1,7 @@
-import { moveStageHandler } from './stage.handler.js';
+import { gameStart } from './game.handler.js';
 
 const handlerMappings = {
-  11: moveStageHandler,
+  2: gameStart,
 };
 
 export default handlerMappings;
