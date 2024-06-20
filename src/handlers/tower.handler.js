6f3e3791c67712ff063f0f40e5c929480bbc8a6f
hyeonseol00 <hyeonseol22@naver.com>
2024-06-20 15:35:33 +0900

    ✨ update : 타워 환불 핸들러 추가

--- src/handlers/tower.handler.js
+++ src/handlers/tower.handler.js
@@ -19,7 +19,7 @@ export const placeTowerHandler = async (userId, payload) => {
   if (userData.gold < commonData.tower_cost)
     return { status: 'fail', message: '돈이 부족해 구매에 실패했습니다!' };
 
-  userData.gold -= 1000;
+  userData.gold -= commonData.tower_cost;
   userData.tower_coordinates.push(payload.newTowerCoordinates);
   await updateUserData(userData);
 
@@ -27,5 +27,23 @@ export const placeTowerHandler = async (userId, payload) => {
     status: 'success',
     message: '구매한 타워가 성공적으로 배치되었습니다.',
     data: userData,
+  };
+};
+
+export const refundTowerHandler = async (userId, payload) => {
+  const userData = await getUserData(userId);
+  const { commonData } = getGameAssets();
+
+  if (userData.tower_coordinates.length <= 0)
+    return { status: 'fail', message: '환불할 수 있는 타워가 없습니다!' };
+
+  userData.gold += commonData.tower_cost;
+  userData.tower_coordinates.pop();
+  await updateUserData(userData);
+
+  return {
+    status: 'success',
+    message: '마지막으로 설치한 타워가 성공적으로 환불되었습니다.',
+    refundTower: userData,
   };
 };
